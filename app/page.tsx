3ff68966d738154256022b21720571aa4821727f
biancadimaano <bianca.fbcd@gmail.com>
2025-02-28 16:48:12 -0800

    feat: Renamed 'WASH' in <h1> element to 'Dashboard' for more descriptiveness. Also modified padding on <main> element

--- app/page.tsx
+++ app/page.tsx
@@ -6,10 +6,10 @@ export default function DashboardPage() {
   return (
     <div className="flex min-h-screen flex-col">
       <DashboardHeader />
-      <main className="flex-1 space-y-4 p-4 md:p-8">
+      <main className="flex-1 space-y-4 px-4 pb-4">
         <div className="flex items-center justify-between">
-          <h1 className="text-3xl font-bold font-display tracking-tight">WASH: Weekly Academic Schedule Helper</h1>
-          <p className="text-lg text-muted-foreground">Finally, something clean in the CS department</p>
+          <h1 className="text-4xl font-bold font-display tracking-tight">Dashboard</h1>
+          {/* <p className="text-lg text-muted-foreground">Finally, something clean in the CS department</p> */}
         </div>
         <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-7">
           <div className="col-span-4">
