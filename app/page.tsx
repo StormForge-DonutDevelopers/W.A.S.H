6bbdce7cd386c62b650831d6cb21487f4db6d02d
biancadimaano <bianca.fbcd@gmail.com>
2025-02-28 17:01:42 -0800

    feat: Removed boldface for h1

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ export default function DashboardPage() {
       <DashboardHeader />
       <main className="flex-1 space-y-4 px-4 pb-4">
         <div className="flex items-center justify-between">
-          <h1 className="text-4xl font-bold font-display tracking-tight">Dashboard</h1>
+          <h1 className="text-4xl font-display tracking-tight">Dashboard</h1>
           {/* <p className="text-lg text-muted-foreground">Finally, something clean in the CS department</p> */}
         </div>
         <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-7">
