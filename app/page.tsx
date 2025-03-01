d843228b011ba7607d07d05ae629e756fc5b508e
biancadimaano <bianca.fbcd@gmail.com>
2025-02-28 16:41:27 -0800

    feat: Changed h1 to display font

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ export default function DashboardPage() {
       <DashboardHeader />
       <main className="flex-1 space-y-4 p-4 md:p-8">
         <div className="flex items-center justify-between">
-          <h1 className="text-3xl font-bold tracking-tight">WASH: Weekly Academic Schedule Helper</h1>
+          <h1 className="text-3xl font-bold font-display tracking-tight">WASH: Weekly Academic Schedule Helper</h1>
           <p className="text-lg text-muted-foreground">Finally, something clean in the CS department</p>
         </div>
         <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-7">
