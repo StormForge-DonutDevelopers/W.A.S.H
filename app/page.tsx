5c4b5f2ba07cc9b1e940daed9d86ea0e3d0df873
biancadimaano <bianca.fbcd@gmail.com>
2025-03-30 11:19:58 -0700

    feat: Implemented images and CSS styling to main page

--- app/page.tsx
+++ app/page.tsx
@@ -12,22 +12,52 @@ import raccoon from '../lib/images/raccoon.png'
 
 export default function DashboardPage() {
   return (
-    <div className="flex min-h-screen flex-col">
-      <DashboardHeader />
-      <main className="flex-1 space-y-4 p-4 md:p-8">
-        <div className="flex items-center justify-between">
-          <h1 className="text-3xl font-bold tracking-tight">WASH: Weekly Academic Schedule Helper</h1>
-          <p className="text-lg text-muted-foreground">Finally, something clean in the CS department</p>
-        </div>
-        <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-7">
-          <div className="col-span-4">
-            <CoursePlanner />
+    <div className="relative">
+      <Image 
+        src={mirror}
+        alt=""
+        className="absolute z-[-1000] left-0 top-0"
+      />
+
+      <div className="flex min-h-screen flex-col mx-[10rem] mb-[5rem]">
+        <main className="flex-1 space-y-4 p-4 md:p-8">
+          <div className="mt-[5rem] ml-[20rem]">
+            <h1 className="text-[5rem] font-display tracking-tight">Hi, Jane!</h1>
+            <p className="text-xl mt-[-0.5rem] mb-[31.5rem]">What are your plans for today?</p>
           </div>
-          <div className="col-span-3">
-            <ChatbotWindow />
+
+          <Image 
+            src={bubbles}
+            alt=""
+            className="absolute z-[-100] left-0 top-[-8.5rem]"
+          />
+
+          <Image 
+            src={duck}
+            alt=""
+            className="absolute z-[-1] w-[16rem] top-[34rem] left-[48%]"
+          />
+{/* 
+          <Image 
+            src={raccoon}
+            alt=""
+            className="absolute z-[-1] top-[34rem] right-[10rem]"
+          /> */}
+
+          <DashboardHeader />
+
+          <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-7">
+            <div className="col-span-4 mt-[10rem]">
+              <h2 className="text-4xl mb-[2rem] text-[#FBFBFB] font-display">Course Planner</h2>
+              <CoursePlanner />
+            </div>
+            <div className="col-span-3 mt-[10rem]">
+              <h2 className="text-4xl mb-[2rem] text-[#FBFBFB] font-display">Course Assistant</h2>
+              <ChatbotWindow />
+            </div>
           </div>
-        </div>
-      </main>
+        </main>
+      </div>
     </div>
   )
 }
