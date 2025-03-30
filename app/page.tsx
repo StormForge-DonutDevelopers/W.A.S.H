b992308f6050baefb610ec6331a3d0b0c1270451
biancadimaano <bianca.fbcd@gmail.com>
2025-03-30 11:29:06 -0700

    fix: Comment out Dashboard Header component

--- app/page.tsx
+++ app/page.tsx
@@ -44,7 +44,7 @@ export default function DashboardPage() {
             className="absolute z-[-1] top-[34rem] right-[10rem]"
           /> */}
 
-          <DashboardHeader />
+          {/* <DashboardHeader /> */}
 
           <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-7">
             <div className="col-span-4 mt-[10rem]">
